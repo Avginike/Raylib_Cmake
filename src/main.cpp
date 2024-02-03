79c7317818a8ea5e953b01109c3f1da2397df232
Avginike <janjuriewicz@gmail.com>
2024-02-03 20:28:50 +0100

    added basic main

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <raylib.h>
+
+int scalefactor = 2;
+int screenWidth = 256 * scalefactor;
+int screenHeight = 240 * scalefactor;
+
+bool ShouldWindowClose = false;
+
+int main()
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    InitWindow(screenWidth, screenHeight, "Project Slime");
+
+    while(!ShouldWindowClose)    // Detect window close button or ESC key
+    {
+        ShouldWindowClose = WindowShouldClose();
+
+        
+    
+
+
+    }
+}
